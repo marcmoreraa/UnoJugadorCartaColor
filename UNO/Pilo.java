18a1bb5af72e6cb8a83088e955009c95dba32723
Marc Morera <mmorera124@boscdelacoma.cat>
2025-03-27 10:29:52 +0100

    Crear Clase Pilo

---
+++ UNO/Pilo.java
@@ -0,0 +1,15 @@
+package UNO;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Pilo {
+    Stack<Carta> cartes= new Stack<Carta>();
+
+    public Stack<Carta> getCartes(){
+        return cartes;
+    }
+
+
+
+}
