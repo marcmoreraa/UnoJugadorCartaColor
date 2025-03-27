80666356bea4bf9fad88f379b4a4cbc920a9f2ae
Marc Morera <mmorera124@boscdelacoma.cat>
2025-03-27 10:29:36 +0100

    Crear Clase Mazo

---
+++ UNO/Mazo.java
@@ -0,0 +1,44 @@
+package UNO;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+
+public class Mazo {
+    Stack<Carta> cartes= new Stack<Carta>();
+
+    public Mazo() {
+        for (Carta.Color color : Carta.Color.values()){
+            cartes.push(new Carta(0,color));
+            for (int i = 1; i<=9; i++){
+                cartes.push(new Carta(i,color));
+                cartes.push(new Carta(i,color));
+            }
+        }
+
+    }
+
+    public ArrayList<Carta> getCartes(){
+        return new ArrayList<>(cartes);
+    }
+
+    public Carta agafarCarta(){
+        return cartes.pop();
+    }
+    public void barrejar(){
+        Collections.shuffle(cartes);
+    }
+    public void reiniciar(Pilo pilo){
+        Carta ultimaCarta;
+        ultimaCarta = pilo.getCartes().pop();
+
+        while (!pilo.getCartes().empty()){
+            cartes.push(pilo.getCartes().pop());
+        }
+
+        pilo.getCartes().push(ultimaCarta);
+
+    }
+
+}
